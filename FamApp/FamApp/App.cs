0d2d85bdf898855ae95c38d18a0ce7a377dafe99
htcstudent <htcstudent@EH255WD14>
2017-02-06 18:36:09 -0800

    Add project files.

---
+++ FamApp/FamApp/App.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace FamApp
+{
+    public class App : Application
+    {
+        public App()
+        {
+            // The root page of your application
+            var content = new ContentPage
+            {
+                Title = "FamApp",
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.Center,
+                    Children = {
+                        new Label {
+                            HorizontalTextAlignment = TextAlignment.Center,
+                            Text = "Welcome to Xamarin Forms!"
+                        }
+                    }
+                }
+            };
+
+            var FamilyPage = new ContentPage
+            {
+
+
+            };
+
+            MainPage = new NavigationPage(content);
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
